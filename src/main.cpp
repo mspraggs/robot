318e0d4d40448a408c404df358641609f984236f
Matt Spraggs <matthew.spraggs@gmail.com>
2015-02-22 21:35:52 +0000

    Added some message handling and html file

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,32 @@ int main(int argc, char* argv[])
   robot::Robot robot(io, 9600, argv[1]);
 
   crow::SimpleApp app;
-  crow::mustache::set_base(".");
+  crow::mustache::set_base("assets");
 
   CROW_ROUTE(app, "/")
-    ([](){
-      return "About Crow example.";
+    ([]{
+      crow::mustache::context ctx;
+      return crow::mustache::load("index.html").render();
+    });
+
+  CROW_ROUTE(app, "/command").methods("GET"_method, "POST"_method)
+    ([&](const crow::request& req){
+      if (req.body == "forward") {
+        robot.forward();
+      }
+      else if (req.body == "reverse") {
+        robot.reverse();
+      }
+      else if (req.body == "right") {
+        robot.right();
+      }
+      else if (req.body == "left") {
+        robot.left();
+      }
+      else if (req.body == "stop") {
+        robot.stop();
+      }
+      return "";
     });
 
   app.port(8080).multithreaded().run();
